db12f077e78c0133a95e22a2980e728cbe9d969b
David Vaughan <jellyconsumers@gmail.com>
2026-05-03 11:59:43 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,23 @@ fn main() {
      loop {
           print!("$ ");
           io::stdout().flush().unwrap();
-          let mut command = String::new();
-          io::stdin().read_line(&mut command).unwrap();
-          command = command.trim().to_string();
-          if command == "exit"{
-               exit(0);
+          let mut input = String::new();
+          io::stdin().read_line(&mut input).unwrap();
+          let commands: Vec<&str> = input.trim().split_whitespace().collect();
+          match commands.as_slice() {
+               ["add",_] => {} // _ is 1 value
+               ["echo", after @ ..] => {
+                    let output = after.join(" ");
+                    println!("{}", output); //{:?} {:#?}
+               }
+               ["exit",..]  => exit(0),
+               _ =>  println!("{0}: command not found", input) // default for match
           }
-          println!("{0}: command not found", command);
+
      }
 }
+
+// https://www.w3tutorials.net/blog/rust-pattern-matching-over-a-vector/
+// https://buildsoftwaresystems.com/post/rust-match-slice-pattern-matching/
+
+
