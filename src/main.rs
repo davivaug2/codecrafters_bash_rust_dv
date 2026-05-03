4cdf17d7a39893ac0af83e956d074edfcb18fed1
David Vaughan <jellyconsumers@gmail.com>
2026-05-03 03:09:50 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,11 @@ use std::io::{self, Write};
 //use std::io::Read;
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
-     print!("$ ");
-     io::stdout().flush().unwrap();
-     let mut command = String::new();
-     io::stdin().read_line(&mut command).unwrap();
-     //let d3 = command.trim();
-     println!("{0}: command not found", command.trim()); // println!("{}: command not found", command);
-     // io::stdout().flush().unwrap();
-     //io::stdin().read_exact()
-     // windows, the newline delimiter is "\r\n" instead of "\n".
+     loop {
+          print!("$ ");
+          io::stdout().flush().unwrap();
+          let mut command = String::new();
+          io::stdin().read_line(&mut command).unwrap();
+          println!("{0}: command not found", command.trim());
+     }
 }
