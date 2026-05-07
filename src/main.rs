be7558877f1b0d2d7752cb88f147926152c23c89
David Vaughan <jellyconsumers@gmail.com>
2026-05-06 20:49:03 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,9 @@ use std::process::exit;
 use std::env;
 use std::ffi::OsString;
 use std::fs::Permissions;
+
 //use std::fs;
-//use std::os::unix::fs::PermissionsExt;// Cannot find `unix` in `os` [E0433]
+use std::os::unix::fs::PermissionsExt;// Cannot find `unix` in `os` [E0433]
 //`std_internals` is unstable [E0658]
 
 //use std::path::PathBuf;
@@ -44,19 +45,23 @@ fn main() {
         match commands.as_slice() {
             ["add",_] => {} // _ is 1 value
             ["echo", after @ ..] => {
-                println!("{}:", after.join(" ")); //  let output = after.join(" ");//String better print
+                println!("{}", after.join(" ")); //  let output = after.join(" ");//String better print
             }
              ["type", second_command, after @ ..] => {
                  let output = second_command;//after.join(" "); // after[0] // .iter().enumerate() finds inex then  then
                  let output2 = OsString::from(&output);
                  if (predefined_commands.contains(second_command)) {
                      println!("{} is a shell builtin", second_command);
+// full_path.exists() == true ? same as full_path.exists()
                  } else if let Some(path_name) = paths_v.iter().find(|path| {
-                     path.file_name() == Some(output2.as_os_str())
-                         && !std::fs::metadata(path).unwrap().permissions().readonly()
-                 })
+                     let full_path = path.join(second_command);
+                     full_path.exists()
+                         && std::fs::metadata(&full_path).unwrap().permissions().mode() & 0o111 != 0
+                         //metadata(path).unwrap().permissions().
+                 })//
                     {
                      println!("{} is {}", output, path_name.display());
+                        let fdfs =  std::fs::metadata(path_name).unwrap().permissions();
                     }
                  else { println!("{}: not found", second_command); };
              }//type
