396ed682d58064ec17aae6a821ac909e329fbc26
David Vaughan <jellyconsumers@gmail.com>
2026-05-08 09:19:29 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -20,13 +20,22 @@ fn main() -> Result<(), Box<dyn std::error::Error>> { //let args: Vec<String> = 
                 println!("{}", after.join(" ")); //  let output = _after.join(" ");//String better print
             }
             ["pwd", _after @ ..] => {
-                //std::env::home_dir()
+                //
                 let path = std::env::current_dir()?; // env::getcwd().unwrap().to_str().unwrap());
                 println!("{}", path.display());//The current directory is.  // current_dir.to_str().unwrap()
             }
             ["cd", path_parts @ ..] => {
-                let path_dir = path_parts.join(" ");
-                std::env::set_current_dir(&path_dir).unwrap_or_else(|_| println!("cd: {path_dir}: No such file or directory"));
+                let path_dir = path_parts.join(" "); // could do just path_parts no @
+                if  path_dir =="~"{
+                    let home_dir = std::env::home_dir().unwrap_or_default(); // .unwrap().as_os_str()
+                    std::env::set_current_dir(&home_dir).unwrap_or_else(
+                        |_| println!("cd: {:?}: No Home directory found.",&home_dir));
+                }
+                else {
+                    std::env::set_current_dir(&path_dir).unwrap_or_else(
+                        |_a| println!("cd: {path_dir}: No such file or directory"));
+                }
+
 
 
             }
